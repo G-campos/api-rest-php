4d5175af3a4b730ea9a788439c317b2f9fc29049
kontrolbits <gabodecampos@gmail.com>
2020-10-05 12:20:36 -0300

    update

--- index.php
+++ index.php
@@ -9,14 +9,13 @@ $app = new \Slim\App;
 
 /* Padrao PSR7 */
 $app->get('/postagens', function(Request $request, Response $response){
-
+   
    /* Escreve no corpo da resposta utilizando o padrao PSR7 */
    $response->getBody()->write("Listagem de postagens");
-
+   
    return $response;
 });
 
-$app->run();
 
 /*
 Tipos de requisicoes ou Verbos HTTP
@@ -28,6 +27,7 @@ delete -> Deletar dados do servidor (delete)
 
 */
 
+$app->run();
 
 /*Placeholders 
 $app->get('/postagens2', function () {
@@ -37,12 +37,12 @@ $app->get('/postagens2', function () {
 /*paramentos obrigatoriso{} ou opcionais[] 
 $app->get('/usuarios[/{id}]', function ($request, $response) {
    $id = $request->getAttribute('id');
-
+   
    echo "Lista de usuarios ou ID: " . $id;
 });
 
 $app->get('/postagens[/{ano}[/{mes}]]', function ($request, $response) {
-
+   
    $ano = $request->getAttribute('ano');
    $mes = $request->getAttribute('mes');
 
@@ -51,7 +51,7 @@ $app->get('/postagens[/{ano}[/{mes}]]', function ($request, $response) {
 
 /*Estrutura de url +dinamica *
 $app->get('/lista/{itens:.*}', function ($request, $response) {
-
+   
    $itens = $request->getAttribute('itens');
    
    var_dump(explode("/", $itens));
@@ -63,9 +63,9 @@ $app->get('/blog/postagens/{id}', function ($request, $response) {
 })->setName("blog");
 
 $app->get('/meusite', function($request, $response){
-
+   
    $retorno = $this->get("router")->pathFor("blog", ["id" => "10"] );
-
+   
    echo $retorno;
 });
 
